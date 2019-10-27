524fb477ca7812ee0abac939bd3683cf8e763326
Andrii Baliuk <zerksis.break@gmail.com>
2019-10-27 09:58:15 +0200

    Added website

---
+++ precache-manifest.d4959f840259855bf0639e21c2a6a511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1f326b0bc5501b0787c9fe83acba5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8372e1efffc16d1ffe3b",
+    "url": "/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "7cc82bcd8c141fc11484",
+    "url": "/static/css/main.2cf92baf.chunk.css"
+  },
+  {
+    "revision": "8372e1efffc16d1ffe3b",
+    "url": "/static/js/2.5f4f72fb.chunk.js"
+  },
+  {
+    "revision": "7cc82bcd8c141fc11484",
+    "url": "/static/js/main.1b7dca46.chunk.js"
+  },
+  {
+    "revision": "f982efce363040017ed4",
+    "url": "/static/js/runtime-main.1cb65ff4.js"
+  }
+]);
